82fc1a85f5984453145a33ed69d44b5923630637
ksk45 <oooouu1k@gmail.com>
2025-06-23 00:30:04 +0900

    Quizページのコンポーネント作成（Button）

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,3 +1,4 @@
+import Button from '../components/Button/Button.jsx';
 import Display from '../components/Display/Display'
 import quizData from '../data/quiz.js'
 
@@ -10,6 +11,10 @@ export default function QuizPage() {
       <Display>
           {`Q1. ${quizData[quizIndex].question}`}
       </Display>
+      <Button>{quizData[quizIndex].options[0]}</Button>
+      <Button>{quizData[quizIndex].options[1]}</Button>
+      <Button>{quizData[quizIndex].options[2]}</Button>
+      <Button>{quizData[quizIndex].options[3]}</Button>
     </>
   )
 }
