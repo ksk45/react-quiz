fcc1432575fe2bf75e13ce4ecb4c4ec331359e17
ksk45 <oooouu1k@gmail.com>
2025-06-23 00:45:42 +0900

    Buttonイベントの設定

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,20 +1,24 @@
-import Button from '../components/Button/Button.jsx';
-import Display from '../components/Display/Display'
-import quizData from '../data/quiz.js'
+import Button from "../components/Button/Button.jsx";
+import Display from "../components/Display/Display";
+import quizData from "../data/quiz.js";
 
 export default function QuizPage() {
-
   const quizIndex = 0;
+
+  const handleClick = (clickedIndex) => {
+    console.log("clickIndex:", clickedIndex);
+  };
 
   return (
     <>
-      <Display>
-          {`Q1. ${quizData[quizIndex].question}`}
-      </Display>
-      <Button>{quizData[quizIndex].options[0]}</Button>
-      <Button>{quizData[quizIndex].options[1]}</Button>
-      <Button>{quizData[quizIndex].options[2]}</Button>
-      <Button>{quizData[quizIndex].options[3]}</Button>
+      <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>
+      {quizData[quizIndex].options.map((option, index) => {
+        return (
+          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+            {option}
+          </Button>
+        );
+      })}
     </>
-  )
+  );
 }
