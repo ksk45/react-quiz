56cd636c44a562a20c2dd65d2056c72b60a84e33
ksk45 <oooouu1k@gmail.com>
2025-06-23 16:02:15 +0900

    クイズ回答ページの作成

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../components/Button/Button.jsx";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz.js";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../const.js";
 
 export default function QuizPage() {
-  
+
   const [quizIndex, setQuizIndex] = useState(0);
   const [answerLogs, setAnswerLogs] = useState([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
 
   const handleClick = (clickedIndex) => {
     if (clickedIndex === quizData[quizIndex].answerIndex) {
-      setAnswerLogs((prev) => [...prev, true]);
+      setAnswerLogs(prev => [...prev, true]);
     } else {
-      setAnswerLogs((prev) => [...prev, false]);
+      setAnswerLogs(prev => [...prev, false]);
     }
-    setQuizIndex((prev) => prev + 1);
+    setQuizIndex(prev => prev + 1);
   };
+
+  useEffect(() => {
+    if (answerLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answerLogs.filter(answer => {
+        return answer === true;
+      });
+
+      navigation(ROUTES.RESULT, {
+        state: {
+          maxQuizLen: MAX_QUIZ_LEN,
+          correctNumLen: correctNum.length
+        }
+      });
+    }
+  }, [answerLogs, MAX_QUIZ_LEN, navigation]);
 
   return (
     <>
-      <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>
-      {quizData[quizIndex].options.map((option, index) => {
+      {quizData[quizIndex] && <Display>{`Q${quizIndex + 1}. ${quizData[quizIndex].question}`}</Display>}
+      <br />
+      {quizData[quizIndex] && quizData[quizIndex].options.map((option, index) => {
         return (
           <Button key={`option-${index}`} onClick={() => handleClick(index)}>
             {option}
