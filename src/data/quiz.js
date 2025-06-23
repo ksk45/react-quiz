b9432e1cd6a1bf329c9c8363aa85fb3144091a35
ksk45 <oooouu1k@gmail.com>
2025-06-23 17:50:36 +0900

    クイズ用問題をダミーから本番用へ

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -1,28 +1,28 @@
 const quizData = [
   {
-    question: "Question 1",
-    options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+    question: "ノーベル賞の授賞式が行われる都市はどこ？",
+    options: ["ストックホルム", "オスロ", "コペンハーゲン", "ヘルシンキ"],
     answerIndex: 0,
   },
   {
-    question: "Question 2",
-    options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+    question: "元素記号『Ag』は何の元素？",
+    options: ["金", "銀", "銅", "鉛"],
     answerIndex: 1,
   },
   {
-    question: "Question 3",
-    options: ["Select 1", "Select 2", "Select 3", "Select 4"],
-    answerIndex: 2,
+    question: "日本で一番面積が大きい都道府県は？",
+    options: ["岩手県", "北海道", "福島県", "新潟県"],
+    answerIndex: 1,
   },
   {
-    question: "Question 4",
-    options: ["Select 1", "Select 2", "Select 3", "Select 4"],
-    answerIndex: 3,
-  },
-  {
-    question: "Question 5",
-    options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+    question: "ピタゴラスの定理で有名な三角形の辺の比は？",
+    options: ["3:4:5", "2:3:4", "1:2:3", "5:12:13"],
     answerIndex: 0,
+  },
+  {
+    question: "世界で最も人口が多い都市（都市圏）は？（2023年時点）",
+    options: ["ニューヨーク", "上海", "東京", "デリー"],
+    answerIndex: 2,
   },
 ];
 
