148ff3d4fbfc53583bbf564ffc9ee4cc704bee88
ksk45 <oooouu1k@gmail.com>
2025-06-23 17:36:42 +0900

    リザルト画面_カーテンコール機能追加

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,25 +1,35 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ROUTES } from "../const";
 import Result from "../components/Result/Result";
+import Loading from "../components/Loading/Loading";
+import { useState, useEffect } from "react";
 
 export default function ResultPage() {
+  const [active, setActive] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
 
-  if (!location.state) {
-    navigate("/");
-    return null;
-  }
-  const maxQuizLen = location.state.maxQuizLen;
-  const correctNumLen = location.state.correctNumLen;
+  const maxQuizLen = location.state ? location.state.maxQuizLen : 0;
+  const correctNumLen = location.state ? location.state.correctNumLen : 0;
+
+  useEffect(() => {
+    if (!location.state) {
+      navigate(ROUTES.TOP);
+    }
+    setTimeout(() => setActive(true), 3000);
+  }, [location.state, navigate]);
+
+
 
   return (
     <>
+      <Loading active={active} />
       <h1>Result</h1>
       <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
       <br />
       <Link to={ROUTES.QUIZ}>クイズに再挑戦</Link>
-      <br /><br />
+      <br />
+      <br />
       <Link to={ROUTES.TOP}>TOPへ</Link>
     </>
   );
